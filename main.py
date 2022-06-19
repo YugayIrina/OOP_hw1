4991eb0ad33236ed264c6c7ab7f2073223bebcc4
YugayIrina <yugay.irina@gmail.com>
2022-06-19 15:41:53 +0300

    Add some changing (oop_hw3)

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ class Student:
         self.grades = {}
         self.average_rating = float()
 
+    def __str__(self):
+        # Считаем среднюю оценку.
+
+        grades_count = 0
+        courses_in_progress_string = ', '.join(self.courses_in_progress)
+        finished_courses_string = ', '.join(self.finished_courses)
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+        res = f'Имя: {self.name}\n' \
+              f'Фамилия: {self.surname}\n' \
+              f'Средняя оценка за домашнее задание: {self.average_rating}\n' \
+              f'Курсы в процессе обучения: {courses_in_progress_string}\n' \
+              f'Завершенные курсы: {finished_courses_string}'
+        return res
+
     def rate_hw(self, lecturer, course, grade):
         # Студент выставляет оценку лектору. Лектор должен вести именно тот курс, на котором учится данный студент.
 
@@ -18,6 +34,13 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __lt__(self, other):
+        # Сравниваем студентов между собой по средней оценке за домашнии задания.
+        if not isinstance(other, Student):
+            print('Такое сравнение некорректно')
+            return
+        return self.average_rating < other.average_rating
 
 
 class Mentor:
@@ -34,9 +57,25 @@ class Lecturer(Mentor):
         self.average_rating = float()
         self.grades = {}
 
+    def __str__(self):
+        grades_count = 0
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rating}'
+        return res
+
+    def __lt__(self, other):
+        # Сравниваем лекторов между собой по средней оценке за лекции.
+        if not isinstance(other, Lecturer):
+            print('Такое сравнение некорректно')
+            return
+        return self.average_rating < other.average_rating
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
+    # Проверяющий выставляет оценку студенту за домашнее задание. Проверяющий должен быть закреплен именно за этим студентов по данному курсу.
 
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -45,6 +84,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
 
 
 # Создаем лекторов и закрепляем их за курсом
@@ -116,15 +159,59 @@ cool_reviewer_2.rate_hw(student_3, 'Python', 8)
 cool_reviewer_2.rate_hw(student_3, 'Python', 7)
 cool_reviewer_2.rate_hw(student_3, 'Python', 9)
 
-print(f'Выставление оценок лекторам:')
-print('Лектор 1:', f'{best_lecturer_1.name} {best_lecturer_1.surname}', '\n', 'Оценка за лекции:',
-      f'{best_lecturer_1.grades}')
-print('Лектор 2:', f'{best_lecturer_2.name} {best_lecturer_2.surname}', '\n', 'Оценка за лекции:',
-      f'{best_lecturer_2.grades}')
-print('Лектор 3:', f'{best_lecturer_3.name} {best_lecturer_3.surname}', '\n', 'Оценка за лекции:',
-      f'{best_lecturer_3.grades}')
+# Выводим характеристики созданных ревьюеров
+print(f'Перечень ревьюеров:\n\n{cool_reviewer_1}\n\n{cool_reviewer_2}')
 print()
-print(f'Выставление оценок студентам:')
-print('Студент 1:', f'{student_1.name} {student_1.surname}', '\n', 'Оценка за лекции:', f'{student_1.grades}')
-print('Студент 2:', f'{student_2.name} {student_2.surname}', '\n', 'Оценка за лекции:', f'{student_2.grades}')
-print('Студент 3:', f'{student_3.name} {student_3.surname}', '\n', 'Оценка за лекции:', f'{student_3.grades}')
+print()
+
+# Выводим характеристики созданных и оцененых лекторов в требуемом виде
+print(f'Перечень лекторов:\n\n{best_lecturer_1}\n\n{best_lecturer_2}\n\n{best_lecturer_3}')
+print()
+print()
+
+# Выводим характеристики созданных и оцененых студентов в требуемом виде
+print(f'Перечень студентов:\n\n{student_1}\n\n{student_2}\n\n{student_3}')
+print()
+print()
+
+
+# Выводим результат сравнения студентов по средним оценкам за домашние задания
+print(f'Результат сравнения студентов (по средним оценкам за ДЗ): '
+      f'{student_1.name} {student_1.surname} < {student_2.name} {student_2.surname} = {student_1 > student_2}')
+print()
+
+# Выводим результат сравнения лекторов по средним оценкам за лекции
+print(f'Результат сравнения лекторов (по средним оценкам за лекции): '
+      f'{best_lecturer_1.name} {best_lecturer_1.surname} < {best_lecturer_2.name} {best_lecturer_2.surname} = {best_lecturer_1 > best_lecturer_2}')
+print()
+
+# Создаем список студентов
+student_list = [student_1, student_2, student_3]
+
+# Создаем список лекторов
+lecturer_list = [best_lecturer_1, best_lecturer_2, best_lecturer_3]
+
+
+def student_rating(student_list, course_name):
+    # Считаем среднюю оценку по всем студентам за домашнии задания в пределах одного курса.
+
+    sum_all = 0
+    count_all = 0
+    for stud in student_list:
+        if stud.courses_in_progress == [course_name]:
+            sum_all += stud.average_rating
+            count_all += 1
+    average_for_all = sum_all / count_all
+    return average_for_all
+
+
+def lecturer_rating(lecturer_list, course_name):
+    # Создаем функцию для подсчета средней оценки за лекции всех лекторов в рамках курса
+    sum_all = 0
+    count_all = 0
+    for lect in lecturer_list:
+        if lect.courses_attached == [course_name]:
+            sum_all += lect.average_rating
+            count_all += 1
+    average_for_all = sum_all / count_all
+    return average_for_all
